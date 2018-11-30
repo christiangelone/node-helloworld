2039b5c9d7889c023869e0e8efd1e3a2e8363fb4
Christian Angelone <christiangelone@gmail.com>
2018-11-29 23:03:21 -0300

    Adds documentaion defined by code, integrated with swagger ui + other stuff that come up

---
+++ src/api/health/controller/health.controller.docs.ts
@@ -0,0 +1,36 @@
+import Docs from "../../../lib/docs";
+
+class HealthDocs extends Docs {
+
+  
+  constructor(){
+    super()
+  }
+
+  'get /api/health'() {
+    return {
+      tags: ['health'],
+      summary: 'Returns the health status of the API',
+      responses: {
+        '200': {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  status: {
+                    type: 'string',
+                    example: 'OK'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+export const Doc = HealthDocs;
